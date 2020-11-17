70a2a63a52a8a59a7838efa54dbc5a077f591711
nikistefanov <nikolay.n.stefanov@gmail.com>
2020-11-17 12:59:04 +0200

    Frontend form logic;

--- src/quantum-sample-page/src/scripts/scripts.js
+++ src/quantum-sample-page/src/scripts/scripts.js
@@ -1,8 +1,17 @@
 ﻿document.addEventListener("DOMContentLoaded", function() {
-    var formSelector = "get-free-demo";
+    var formHeadingSelector = "get-free-demo";
+    var anchorButtonSelector = "a.btn";
+    var checkboxWrapperSelector = ".form-check";
+    var checboxSelector = "input[type=checkbox]";
+    var submitButtonSelector = "submitButton";
+    var submitFormSelector = "submitForm";
+    var submitSuccessSelector = "submitSuccess";
 
-    document.querySelectorAll("a.btn").forEach(function (anchor) {
-        anchor.href = "#" + formSelector;
+    var checkboxInvalidClass = "invalid";
+    var hiddenClass = "d-none";
+
+    document.querySelectorAll(anchorButtonSelector).forEach(function (anchor) {
+        anchor.href = "#" + formHeadingSelector;
 
         anchor.addEventListener("click", function (e) {
             e.preventDefault();
@@ -12,4 +21,42 @@
             });
         });
     });
+
+    var submitButton = document.getElementById(submitButtonSelector);
+    var submitForm = document.getElementById(submitFormSelector);
+    var privacyPolicyCheckbox = submitForm.querySelector(checboxSelector);
+    var submitSuccess = document.getElementById(submitSuccessSelector);
+
+    submitButton.addEventListener("click", function () {
+        if (!privacyPolicyCheckbox.checkValidity()) {
+            privacyPolicyCheckbox.closest(checkboxWrapperSelector).classList.add(checkboxInvalidClass);
+            privacyPolicyCheckbox.focus();
+        }
+    });
+
+    privacyPolicyCheckbox.addEventListener("change", function (event) {
+        if (event.target.checked) {
+            privacyPolicyCheckbox.closest(checkboxWrapperSelector).classList.remove(checkboxInvalidClass);
+        } else {
+            privacyPolicyCheckbox.closest(checkboxWrapperSelector).classList.add(checkboxInvalidClass);
+        }
+    });
+
+    submitForm.onsubmit = function (event) {
+        event.preventDefault();
+
+        if (submitForm.checkValidity()) {
+            var formData = new FormData(submitForm);
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    submitForm.classList.add(hiddenClass);
+                    submitSuccess.classList.remove(hiddenClass);
+                }
+            };
+            xhr.open("post", "/FormValues", true);
+            xhr.send(new URLSearchParams(formData));
+            return false;
+        }
+    };
 });
