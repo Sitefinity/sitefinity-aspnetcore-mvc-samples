8ffac5fc400170b066b9f12a58e36d2d38b9173f
Nikolai Novkirishki <Novkirishki@users.noreply.github.com>
2022-01-26 13:51:00 +0200

    add form widget sample

---
+++ src/form-hidden-input/form-hidden-input/form-hidden-input/Entities/FormHiddenInputEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace form_hidden_input.Entities
+{
+    public class FormHiddenInputEntity
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ src/form-hidden-input/form-hidden-input/form-hidden-input/ViewComponents/FormHiddenInputViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using form_hidden_input.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Progress.Sitefinity.AspNetCore.ViewComponents;
+
+namespace form_hidden_input.ViewComponents
+{
+    /// <summary>
+    /// Test widget with different kind of restrictions for its properties.
+    /// </summary>
+    [SitefinityFormWidget("Hidden", Title = "Hidden input", EmptyIconText = "Custom empty text", EmptyIcon = "eye-slash")]
+    public class FormHiddenInputViewComponent : ViewComponent
+    {
+        /// <summary>
+        /// Invokes the view.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public IViewComponentResult Invoke(IViewComponentContext<FormHiddenInputEntity> context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return this.View(context.Entity);
+        }
+    }
+}
