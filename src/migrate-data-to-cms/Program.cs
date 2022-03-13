5ccf6b536b702a5d2347008bdab866927e85fdd7
Martin Gebov <gebov@progress.com>
2022-03-13 14:28:37 +0200

    wip

---
+++ src/migrate-data-to-cms/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Progress.Sitefinity.RestSdk;
+using Progress.Sitefinity.RestSdk.Client;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Progress.Sitefinity.RestSdk.Dto;
+using IdentityModel.Client;
+
+namespace migrate_data_to_cms
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var config = new SitefinityConfig()
+            {
+                ClientId = "your_client_id",
+                ClientSecret = "your_client_secret",
+                Username = "your_username",
+                Password = "your_password",
+                Url = "http://localhost/api/default"
+            };
+
+            var httpClient = CreateClient(config);
+            var restClient = new RestClient(httpClient);
+            MigrateContent(restClient).Wait();
+        }
+
+        public static async Task MigrateContent(IRestClient restClient)
+        {
+            var newsDto = new NewsDto()
+            {
+                Title = "test"
+            };
+
+            await restClient.CreateItem(newsDto);
+
+            foreach (var newsItem in GetNewsSource())
+            {
+                await restClient.CreateItem(newsItem);
+            }
+        }
+
+        private static IEnumerable<NewsDto> GetNewsSource()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var newsDto = new NewsDto()
+                {
+                    Title = "test" + Guid.NewGuid().ToString(),
+                };
+
+                yield return newsDto;
+            }
+        }
+
+        private static HttpClient CreateClient(SitefinityConfig config)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(config.Url);
+            var token = GetAuthenticationToken(client, config);
+            if (token.IsError)
+            {
+                throw new InvalidOperationException(token.Error);
+            }
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(token.TokenType, token.AccessToken);
+
+            return client;
+        }
+
+        private static TokenResponse GetAuthenticationToken(HttpClient client, SitefinityConfig config)
+        {
+            var baseUri = client.BaseAddress;
+            var baseAddress = baseUri.GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped);
+            var tokenAddress = $"{baseAddress}/sitefinity/oauth/token";
+            using (var token = new PasswordTokenRequest()
+            {
+                Address = tokenAddress,
+                ClientId = config.ClientId,
+                ClientSecret = config.ClientSecret,
+                Scope = "oauth",
+
+                UserName = config.Username,
+                Password = config.Password,
+                Method = HttpMethod.Post,
+            })
+            {
+                var tokenResponse = client.RequestPasswordTokenAsync(token).Result;
+                return tokenResponse;
+            }
+        }
+    }
+}
---
+++ src/migrate-data-to-cms/SitefinityConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Progress.Sitefinity.RestSdk;
+using Progress.Sitefinity.RestSdk.Client;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace migrate_data_to_cms
+{
+    public class SitefinityConfig
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Url { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+    }
+}
