d502aa28aeb6027f8d52a97cc7bca6cbaf678315
gebov <martin.gebov@gmail.com>
2022-09-30 11:00:53 +0300

    update with latest

--- src/content-selectors/ViewComponents/SelectorDemoUsageViewComponent.cs
+++ src/content-selectors/ViewComponents/SelectorDemoUsageViewComponent.cs
@@ -1,8 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using content_selectors.Entities.SelectorDemoUsage;
-using content_selectors.Models.SelectorDemoUsage;
+using content_selectors.ViewModels.SelectorDemoUsage;
 using Microsoft.AspNetCore.Mvc;
+using Progress.Sitefinity.AspNetCore.RestSdk;
 using Progress.Sitefinity.AspNetCore.ViewComponents;
+using Progress.Sitefinity.AspNetCore.Web;
+using Progress.Sitefinity.RestSdk;
+using Progress.Sitefinity.RestSdk.Dto;
+using Progress.Sitefinity.RestSdk.Exceptions;
 
 namespace content_selectors.ViewComponents
 {
@@ -12,15 +20,16 @@ namespace content_selectors.ViewComponents
     [SitefinityWidget]
     public class SelectorDemoUsageViewComponent : ViewComponent
     {
-        private ISelectorDemoUsageModel model;
+        private IRestClient restClient;
+        private IRenderContext renderContext;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SelectorDemoUsageViewComponent"/> class.
         /// </summary>
-        /// <param name="model">The model.</param>
-        public SelectorDemoUsageViewComponent(ISelectorDemoUsageModel model)
+        public SelectorDemoUsageViewComponent(IRestClient restClient, IRenderContext renderContext)
         {
-            this.model = model;
+            this.restClient = restClient;
+            this.renderContext = renderContext;
         }
 
         /// <summary>
@@ -30,7 +39,108 @@ namespace content_selectors.ViewComponents
         /// <returns>The view component result.</returns>
         public async Task<IViewComponentResult> InvokeAsync(IViewComponentContext<SelectorDemoEntity> context)
         {
-            var viewModels = await this.model.GetViewModels(context.Entity);
+            var viewModels = new List<ItemCollection>();
+
+            // GetItems is an extension method from the class RestClientExtensions in the namespace Progress.Sitefinity.RestSdk
+            // In order to use it the generic type must inherit from ISdkItem
+            if (context.Entity.News.HasSelectedItems())
+            {
+                var newsItems = await this.restClient.GetItems<SdkItem>(context.Entity.News);
+                viewModels.Add(new ItemCollection()
+                {
+                    GroupTitle = "News items draft",
+                    Items = newsItems.Items.ToArray(),
+                });
+            }
+
+            if (context.Entity.NewsLive.HasSelectedItems())
+            {
+                var newsItemsLive = await this.restClient.GetItems<SdkItem>(context.Entity.NewsLive);
+                viewModels.Add(new ItemCollection()
+                {
+                    GroupTitle = "News items live",
+                    Items = newsItemsLive.Items.ToArray(),
+                });
+            }
+
+            if (context.Entity.Page.HasSelectedItems())
+            {
+                 var page = await this.restClient.GetItems<SdkItem>(context.Entity.Page);
+                viewModels.Add(new ItemCollection()
+                {
+                    GroupTitle = "Page",
+                    Items = page.Items.ToArray(),
+                });
+            }
+
+            if (context.Entity.Images.HasSelectedItems())
+            {
+                var images = await this.restClient.GetItems<SdkItem>(context.Entity.Images);
+                viewModels.Add(new ItemCollection()
+                {
+                    GroupTitle = "Image",
+                    Items = images.Items.ToArray()
+                });
+            }
+
+            if (context.Entity.Tags.HasSelectedItems())
+            {
+                var tags = await this.restClient.GetItems<SdkItem>(context.Entity.Tags);
+                viewModels.Add(new ItemCollection()
+                {
+                    GroupTitle = "Tags",
+                    Items = tags.Items.ToArray()
+                });
+            }
+
+            try
+            {
+                if (context.Entity.GeographicalRegions.HasSelectedItems())
+                {
+                    var geoRegions = await this.restClient.GetItems<SdkItem>(context.Entity.GeographicalRegions).ConfigureAwait(true);
+                    viewModels.Add(new ItemCollection()
+                    {
+                        GroupTitle = "Geographical Regions",
+                        Items = geoRegions.Items.ToArray(),
+                    });
+                }
+            }
+            catch (ErrorCodeException err)
+            {
+                if (this.renderContext.IsEdit)
+                {
+                    viewModels.Add(new ItemCollection()
+                    {
+                        GroupTitle = $"Geographical Regions - {err.Message}",
+                        Items = Array.Empty<SdkItem>(),
+                    });
+                }
+            }
+
+            try
+            {
+                if (context.Entity.PressReleases.HasSelectedItems())
+                {
+                    var pressReleases = await this.restClient.GetItems<SdkItem>(context.Entity.PressReleases);
+                    viewModels.Add(new ItemCollection()
+                    {
+                        GroupTitle = "Press Releases",
+                        Items = pressReleases.Items.ToArray(),
+                    });
+                }
+            }
+            catch (Exception err)
+            {
+                if (this.renderContext.IsEdit)
+                {
+                    viewModels.Add(new ItemCollection()
+                    {
+                        GroupTitle = $"Press Releases - {err.Message}",
+                        Items = Array.Empty<SdkItem>(),
+                    });
+                }
+            }
+
             return this.View(viewModels);
         }
     }
