cf44bfff7f6207b39e28127a48cf1da172f8c69d
Gebov <gebov@progress.com>
2022-10-05 12:02:50 +0300

    Html Sanitizer Override Sample (#91)

---
+++ src/html-sanitizer/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using Progress.Sitefinity.AspNetCore;
+using Progress.Sitefinity.AspNetCore.FormWidgets;
+using Progress.Sitefinity.AspNetCore.Web.Security;
+using html_sanitizer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSitefinity();
+builder.Services.AddViewComponentModels();
+builder.Services.AddFormViewComponentModels();
+
+builder.Services.AddSingleton<IHtmlSanitizer, CustomHtmlSanitizer>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSitefinity();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapSitefinityEndpoints();
+});
+
+app.Run();
