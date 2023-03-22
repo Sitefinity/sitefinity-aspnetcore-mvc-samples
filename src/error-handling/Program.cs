d056076c22d3ccd857ec13e83a9e5595b196f62a
gebov <martin.gebov@gmail.com>
2023-03-22 14:37:24 +0200

    add serilog logging

--- src/error-handling/Program.cs
+++ src/error-handling/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 using Progress.Sitefinity.AspNetCore;
 using Progress.Sitefinity.AspNetCore.FormWidgets;
-using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .WriteTo.File("log.txt"));
 
 // Add services to the container.
 builder.Services.AddSitefinity();
@@ -25,16 +29,11 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-
+app.UseSerilogRequestLogging();
 app.UseStatusCodePagesWithReExecute("/Error", "?statusCode={0}");
 app.UseStaticFiles();
 app.UseRouting();
 app.MapRazorPages();
-
-app.Use(async (context, next) =>
-{
-    await next();
-});
 
 app.UseSitefinity();
 
--- src/error-handling/README.md
+++ src/error-handling/README.md
@@ -1,6 +1,10 @@
 # Error pages
 
+## Integration
 Shows integration between ASP.NET Core error pages middleware and status code middleware for integration with the 
 Sitefinity .NET Core renderer. The implementation itself has no customizations and adheres to the official [MS docs]() on handling error pages.
 
 The example uses RazorPages to show the custom error pages with their original status code.
+
+## Logging
+The sample uses Serilog for customized logging. Again there are no customizations made, just the standard implementation integrated with [this Serilog sample](https://github.com/datalust/dotnet6-serilog-example)
