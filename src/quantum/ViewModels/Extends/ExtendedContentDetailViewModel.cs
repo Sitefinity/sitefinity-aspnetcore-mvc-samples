7fc7ef58fbd42c82afccb27226f448ebfcf01b25
Aleks Damyanov <142979250+adamyano@users.noreply.github.com>
2023-10-20 10:10:36 +0300

    Extend Content List Details page (#142)

---
+++ src/quantum/ViewModels/Extends/ExtendedContentDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Progress.Sitefinity.AspNetCore.Widgets.Models.ContentList;
+using Progress.Sitefinity.RestSdk.Dto;
+
+namespace Renderer.ViewModels.Extends
+{
+    public class ExtendedContentDetailViewModel : ContentDetailViewModel
+    {
+        public ExtendedContentDetailViewModel(ContentDetailViewModel source) : base(source.Item)
+        {
+            this.CssClass = source.CssClass;
+        }
+
+        public string WhereLabel { get; set; }
+
+        public string WhenLabel { get; set; }
+    }
+}
