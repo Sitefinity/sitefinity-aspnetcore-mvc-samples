c24b1c1d415c9c52e97cb05fb6a3e343bbd2c8f9
Decho Dechev <ddechev@progress.com>
2024-04-18 17:04:46 +0300

    Save status in bash script

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+#
+# Build All Projects
+
+declare -i status=0
+
 for proj in ../src/*; do
  echo $proj
  cd $proj
  dotnet build
+ status=$(($status+$?))
  cd ../../build
+ echo $status
 done
+
+exit $status
