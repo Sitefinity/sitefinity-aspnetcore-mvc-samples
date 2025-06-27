e4408b6d0d607381108d1f399f466a8b68e77259
hasan <hasan@progress.com>
2025-06-27 10:26:51 +0300

    Update language selector example

--- src/language-selector/Models/LanguageSelector/LanguageSelectorModel.cs
+++ src/language-selector/Models/LanguageSelector/LanguageSelectorModel.cs
@@ -6,6 +6,7 @@ using language_selector.ViewModels.LanguageSelector;
 using Progress.Sitefinity.AspNetCore.Web;
 using Progress.Sitefinity.RestSdk;
 using Progress.Sitefinity.RestSdk.Clients.Pages.Dto;
+using Microsoft.AspNetCore.Http;
 
 namespace language_selector.Models.LanguageSelector
 {
@@ -13,37 +14,41 @@ namespace language_selector.Models.LanguageSelector
     {
         private IRequestContext requestContext;
         private IRestClient restClient;
+        private IHttpContextAccessor httpContextAccessor;
 
-        public LanguageSelectorModel(IRequestContext requestContext, IRestClient restClient)
+        public LanguageSelectorModel(IRequestContext requestContext, IRestClient restClient, IHttpContextAccessor httpContextAccessor)
         {
             this.requestContext = requestContext;
             this.restClient = restClient;
+            this.httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<LanguageSelectorViewModel> GetViewModel(LanguageSelectorEntity entity)
         {
-            var cultures = this.requestContext.Site.Cultures;
-
+            var request = this.httpContextAccessor.HttpContext.Request;
             var viewModel = new LanguageSelectorViewModel();
             var culturePageMap = new Dictionary<string, Task<PageNodeDto>>();
             if (this.requestContext.PageNode != null)
             {
+                string[] pageNodeAvailableLanguages = this.requestContext.PageNode.GetValue<string[]>("AvailableLanguages");
+                var detailItem = this.requestContext.Model.DetailItem;
+
                 var batchBuilder = this.restClient.StartBatch();
-                foreach (var culture in cultures)
+                foreach (var language in pageNodeAvailableLanguages)
                 {
                     var response = batchBuilder.GetItem<PageNodeDto>(new GetItemArgs()
                     {
                         Id = this.requestContext.PageNode.Id,
                         Provider = this.requestContext.PageNode.Provider,
-                        Culture = culture.Name,
+                        Culture = language,
                     });
-                    culturePageMap.Add(culture.Name, response);
+                    culturePageMap.Add(language, response);
                 }
 
                 await batchBuilder.Execute();
             }
 
-
+            var cultures = this.requestContext.Site.Cultures;
             foreach (var culture in cultures)
             {
                 var ci = CultureInfo.GetCultureInfo(culture.Name);
